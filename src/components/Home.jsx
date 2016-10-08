dae8115cccb91cf608b095559713b7e4c273fe67
wh11e7rue <wh11e7rue@icloud.com>
2016-10-09 05:35:21 +0900

    Implement screen transition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {browserHistory} from 'react-router'
 
 
+// Milkcocoaからのイベントを待ち受け、それに応じて/subtitle, /conan, /camera等にリダイレクトさせるコンポーネント
 class Home extends React.Component {
-  // Milkcocoaからのイベントを待ち受け、それに応じて/subtitle, /conan, /camera等にリダイレクトさせるコンポーネント
+  static onMilkCocoa(res) {
+    switch (res.value.trigger) {
+      case 'cap':
+        browserHistory.push('/subtitle')
+        return
+      case 'photo':
+        browserHistory.push('/camera')
+        return
+      default:
+        console.error('weird event', res)
+    }
+  }
+
   render() {
-    return <span>UNDER CONSTRUCTION</span>
+    return <span>アプリが起動されるのを待っています...</span>
   }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Router, Route, browserHistory} from 'react-router'
+import {Router, browserHistory} from 'react-router'
 import Home from './components/Home'
 /* import Subtitle from './components/Subtitle'*/
 /* import Conan from './components/Conan'
  * import Camera from './components/Camera'*/
 
 
-const routing = [
-  {
-    path: '/',
-    component: Home
+class App extends React.Component {
+  constructor() {
+    super()
+    this.currentRoute = Home
+    this.routes = [
+      {
+        path: '/',
+        component: Home,
+        onEnter: () => this.currentRoute = Home
+      }
+    ]
   }
-]
+
+  componentDidMount() {
+    const milkcocoa = new MilkCocoa('woodiu1f1mcm.mlkcca.com')
+    const ds = milkcocoa.dataStore('test-ds')
+    ds.on('send', res => {
+      switch(res.value.trigger) {
+        case 'home':
+          browserHistory.push('/')
+          return
+        default:
+          this.currentRoute.onMilkCocoa(res)
+          return
+      }
+    })
+  }
+
+  render() {
+    return <Router history={browserHistory} routes={this.routes}/>
+  }
+}
 
 render(
-  <Router history={browserHistory} routes={routing}/>,
+  <App/>,
   document.getElementsByTagName('main')[0]
 )
