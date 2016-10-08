8811f51e1e5d9283d51440d687986a0c3893e2e7
_Ghostrick_ <leafia.nagi@gmail.com>
2016-10-09 03:49:20 +0900

    add(sub page)

--- front/phone/js/main.js
+++ front/phone/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+
 	var milkcocoa = new MilkCocoa('woodiu1f1mcm.mlkcca.com');
 
 	var ds = milkcocoa.dataStore('test-ds');
@@ -10,5 +11,22 @@ $(function(){
 	$(".action-menu > li > p").on("click",function(){
 		action = $(this).attr("action");
 		ds.send({trigger : action});
+
+		// transition page .
+		var now  = $("section > div.show");
+		var next = $("section > div." + action);
+
+		$(now).addClass("hide").removeClass("show");
+		$(next).addClass("show");
+	});
+
+	$(".back span").on("click",function(){
+		var now  = $("section > div.show");
+		var prev = $("section > div.hide");
+
+		$(now).removeClass("show");
+		$(prev).removeClass("hide").addClass("show");
+
+		ds.send({trigger : "home"});
 	});
 });
